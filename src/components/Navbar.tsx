9f79d819455ffe67a9da9a7f8a6287674ae80ca3
dmidence <40371373+dmidence@users.noreply.github.com>
2022-10-30 22:04:08 -0600

    Navbar custom

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,13 +1,16 @@
 import React from "react";
-
+import logo from "../assets/img/logo3.png";
 export default function Navbar() {
   return (
-    <nav className="navbar main-navbar ">
+    <nav className="navbar main-navbar  ">
       <div className="container-fluid">
-        <a className="navbar-brand">Navbar</a>
+        <a className="navbar-brand">
+          <img className="img-fluid img-brand" src={logo} alt="" />
+          <span className="text-white">NAPP</span>
+        </a>
         <div className="d-flex" role="search">
-          <button className="btn btn-outline-success" type="submit">
-            Search
+          <button className="btn text-white " type="submit">
+            <strong> Iniciar Sesion</strong>
           </button>
         </div>
       </div>
