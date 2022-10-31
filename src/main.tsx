3a50df00db0e8f7e539b18ecf78d56c9ce002de7
dmidence <40371373+dmidence@users.noreply.github.com>
2022-10-30 18:21:14 -0600

    first commit

---
+++ src/main.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Landing from "./pages/landing/landing";
+import "./index.css";
+
+import { createBrowserRouter, RouterProvider, Route } from "react-router-dom";
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Landing></Landing>,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <RouterProvider router={router} />
+);
---
+++ src/pages/landing/landing.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./landing.scss";
+export default function Landing() {
+  return (
+    <>
+      <div className="test">Landing</div>
+      <button className="btn btn-primary">Botonkk </button>
+    </>
+  );
+}
